ca0e07300dc6159e19a5cec88bad0324e63422e9
codersait <codersait@gmail.com>
2021-08-23 16:47:15 +0300

    video 6

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar'
+import Home from './Home'
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='content'>
+        <Home />
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus fugiat dolorum, est similique soluta pariatur, perspiciatis porro obcaecati harum voluptatum hic ullam consectetur, velit magnam? Voluptatem vel, fugiat culpa assumenda temporibus atque magni qui id at ipsum molestias architecto unde pariatur porro quam. Corporis porro beatae nulla est mollitia incidunt.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
